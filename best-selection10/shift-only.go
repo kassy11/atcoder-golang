7d781b4a8bf7607c015e2d158e57da18ff65d0ce
kassy11 <kotaro11kassy@gmail.com>
2020-05-30 23:06:02 +0900

    shift-only

---
+++ best-selection10/shift-only.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main(){
+	var n, count int
+	fmt.Scan(&n)
+	nums := make([]int, n)
+
+	for i:=0; i<n; i++{
+		fmt.Scan(&nums[i])
+	}
+
+	flag := true
+	for flag{
+		for i:=0; i<n; i++{
+			if nums[i] % 2 == 0{
+				nums[i] /= 2
+			}else{
+				flag = false
+			}
+		}
+		if flag{
+			count++
+		}
+	}
+
+	fmt.Println(count)
+
+}
