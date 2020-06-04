1caaf1e8e1efce3e76ff503f06a21c39fd20e84b
kassy11 <kotaro11kassy@gmail.com>
2020-06-04 10:58:50 +0900

    161 b finish!

--- abc161/b161.go
+++ abc161/b161.go
@@ -5,34 +5,49 @@ import (
 	"sort"
 )
 
-func main() {
-	var n, m int
-	fmt.Scan(&n, &m)
-	points := make([]int, n)
+func sliceUnique(target []int) (unique []int) {
+	m := map[int]bool{}
 
-	for i:=0; i<n; i++{
-		fmt.Scan(&points[i])
-	}
-	sum := 0
-	for _, point := range points{
-		sum += point
-	}
-	chosenable := sum / 4 * m
-
-	sort.Sort(sort.Reverse(sort.IntSlice(points)))
-
-	count := 0
-	for l:=0; l<m; l++{
-		if points[l] > chosenable{
-			count ++
+	for _, v := range target {
+		if !m[v] {
+			m[v] = true
+			unique = append(unique, v)
 		}
 	}
 
-	if count == m{
-		fmt.Println("Yes")
-	}else{
-		fmt.Println("No")
+	return unique
+}
+
+func main() {
+	var n, m int
+	fmt.Scan(&n, &m)
+	if m > n {
+		return
 	}
+	points := make([]int, n)
+	sum := 0
+
+	for i:=0; i<n; i++{
+		fmt.Scan(&points[i])
+		sum += points[i]
+	}
+
+
+	sort.Sort(sort.Reverse(sort.IntSlice(points)))
+	unique_points := sliceUnique(points)
+
+	count := 0
+	for i := 0; i < n; i++ {
+		if unique_points[i]*4*m >= sum {
+			count++
+		}
+	}
+	if count >= m {
+		fmt.Println("Yes")
+		return
+	}
+
+	fmt.Println("No")
 
 
 }
