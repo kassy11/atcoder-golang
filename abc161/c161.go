8811f65bc535621658c55af475ad2fadd200dfb7
kassy11 <kotaro11kassy@gmail.com>
2020-06-04 11:18:13 +0900

    161 c finish!

--- abc161/c161.go
+++ abc161/c161.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var n, k int
+	fmt.Scan(&n, &k)
+
+	if k > n && (k- n) > n {
+		fmt.Println(n)
+	}else{
+		t1 := n % k
+		t2 := k - t1
+		if t1 > t2 {
+			fmt.Println(t2)
+		}else{
+			fmt.Println(t1)
+		}
+	}
 }
