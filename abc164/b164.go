5dc1868dcbcac1ac7c8c169cb29e5f6127ed1ff6
kassy11 <kotaro11kassy@gmail.com>
2020-06-06 15:10:12 +0900

    164 b finish!

--- abc164/b164.go
+++ abc164/b164.go
@@ -5,10 +5,20 @@ import "fmt"
 func main() {
 	var a, b, c, d int
 	fmt.Scan(&a, &b, &c, &d)
+	for{
+		c -= b
 
-	for b >= 0{
-		for a >= 0{
-			
+		if c <= 0{
+			fmt.Println("Yes")
+			break
+		}
+
+		a -= d
+
+		if a <= 0{
+			fmt.Println("No")
+			break
 		}
 	}
+
 }
