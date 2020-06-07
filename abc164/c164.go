d53fbffcadf23987b1aabc978a42b0f8077b65fb
kassy11 <kotaro11kassy@gmail.com>
2020-06-07 15:24:52 +0900

    165 a~b finish!

--- abc164/c164.go
+++ abc164/c164.go
@@ -8,14 +8,14 @@ import (
 func main() {
 	var n int
 	fmt.Scan(&n)
-	goods := make([]string, n)
+	goods := make([]int, n)
 
 	count := 1
 	for i:=0; i<n; i++{
 		fmt.Scan(&goods[i])
 	}
 
-	sort.Strings(goods)
+	sort.Ints(goods)
 
 	for l:=1; l<n; l++{
 		if goods[l-1] != goods[l] {
