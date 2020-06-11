b6ed405cfe2a8076d2ea809b90bde2df6a87f127
kassy11 <kotaro11kassy@gmail.com>
2020-06-11 17:18:14 +0900

    finish breakingnum!

---
+++ bootcamp-easy100/shift-only.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	nums := make([]int, n)
+	for i:=0; i<n; i++{
+		fmt.Scan(&nums[i])
+	}
+
+	count := 0
+	flag := true
+	for flag{
+		for l:=0; l<n; l++{
+			if nums[l] % 2 == 0{
+				nums[l] = nums[l] / 2
+			}else{
+				flag = false
+			}
+		}
+		if flag{
+			count++
+		}
+	}
+	fmt.Println(count)
+}
