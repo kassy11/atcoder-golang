468c6bff0d94c2565857df83f3e9c2a61a18e25c
kassy11 <kotaro11kassy@gmail.com>
2020-06-21 23:02:38 +0900

    finish abc171 a-c

---
+++ abc171/b171.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var n, k int
+	fmt.Scan(&n, &k)
+	if k > n{
+		return
+	}
+	p := make([]int, n)
+	for i:=0; i<n; i++{
+		fmt.Scan(&p[i])
+	}
+
+	sort.Ints(p)
+
+	sum := 0
+	for i:=0; i<k; i++{
+		sum += p[i]
+	}
+
+	fmt.Println(sum)
+}
---
+++ abc171/c171.go
@@ -0,0 +1,24 @@
+
+package main
+
+import (
+"fmt"
+)
+
+func main() {
+	var n int64
+	fmt.Scan(&n)
+	var S []int64
+	for n > 0 {
+		n -= 1
+		S = append(S, n%26)
+		n /= 26
+	}
+	
+	fmt.Println(S)
+
+	for i := range S {
+		fmt.Print(string("a"[0] + byte(S[len(S)-i-1])))
+	}
+	fmt.Println()
+}
