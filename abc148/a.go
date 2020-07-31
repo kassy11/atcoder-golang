e330b933e096f4b35d2fa534ce9226a29541928d
kassy11 <kotaro11kassy@gmail.com>
2020-07-31 11:20:45 +0900

    finish 148 a~c

---
+++ abc148/a.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+	if a + b == 3{
+		fmt.Println(3)
+	}else if a + b == 4{
+		fmt.Println(2)
+	}else if a + b == 5{
+		fmt.Println(1)
+	}else{
+		fmt.Println("error")
+	}
+}
