6e8505ecf9cc48cfa8e8e2ace8176b8ae35abb37
kassy11 <kotaro11kassy@gmail.com>
2020-08-03 19:08:32 +0900

    finish aoj-intro 35

--- aoj-intro/35.go
+++ aoj-intro/35.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	
+	var n, tpoint, hpoint int
+	fmt.Scan(&n)
+	taro := make([]string, n)
+	hanako := make([]string, n)
+	for i:=0; i<n; i++{
+		fmt.Scan(&taro[i], &hanako[i])
+		taro[i] = strings.ToLower(taro[i])
+		hanako[i] = strings.ToLower(hanako[i])
+
+		if taro[i] > hanako[i]{
+			tpoint += 3
+		}else if taro[i] == hanako[i]{
+			tpoint++
+			hpoint++
+		}else{
+			hpoint += 3
+		}
+	}
+
+	fmt.Println(tpoint, hpoint)
 }
