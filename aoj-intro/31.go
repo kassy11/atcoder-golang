49e344c2e31e1a0b2d7ea1bbc1b439b026b769ac
kassy11 <kotaro11kassy@gmail.com>
2020-08-03 12:50:42 +0900

    finish aoj-intro topic8-31

--- aoj-intro/31.go
+++ aoj-intro/31.go
@@ -1,22 +1,34 @@
-//package main
-//
-//import (
-//	"bufio"
-//	"fmt"
-//	"os"
-//)
-//
-//func main() {
-//	scanner := bufio.NewScanner(os.Stdin)
-//	scanner.Scan()
-//	str := scanner.Text()
-//
-//	result := make(map[rune]int, 50)
-//	for _, s := range str{
-//		result[s]++
-//	}
-//
-//	for i, s := range result{
-//		fmt.Printf("%s : %d\n", s, i)
-//	}
-//}
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewReader(os.Stdin)
+
+	result := make(map[string]int, 50)
+
+	// アルファベットでmapを初期化する
+	for i:=0; i<26; i++{
+		result[string('a'+i)] = 0
+	}
+
+	for{
+		str, err := scanner.ReadString('\n')
+		if err == io.EOF{
+			break
+		}
+		for _, s := range str{
+			result[strings.ToLower(string(s))]++
+		}
+	}
+
+	for i:=0; i<26; i++{
+		fmt.Printf("%s : %d\n", string('a'+i), result[string('a'+i)])
+	}
+}
